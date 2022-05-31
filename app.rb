a9e048e175e8daa02d821e013fddbf423781a766
Anh Tran <anh.t.tran151@gmail.com>
2022-05-31 17:33:26 +0000

    adjusted /cake.json route

--- app.rb
+++ app.rb
@@ -7,13 +7,8 @@ get '/' do
 end
 
 get '/cake.json' do
-  h = Hash.new
   file = File.open("cake.list") 
   file_data = file.readlines.map(&:chomp)
 
-  file_data.each.with_index(1) do |x, index|
-    h[index] = x
-  end
-
-  h.to_json
+  file_data.to_json
 end
