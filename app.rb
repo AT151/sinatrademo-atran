d77c1d6532865759343adf8f2323b52d3d1131ee
Anh Tran <anh.t.tran151@gmail.com>
2022-05-31 17:18:57 +0000

    implemented cake.json route and built index view from cake.json

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'sinatra/json'
+require 'json'
 
 get '/' do
   erb :index
+end
+
+get '/cake.json' do
+  h = Hash.new
+  file = File.open("cake.list") 
+  file_data = file.readlines.map(&:chomp)
+
+  file_data.each.with_index(1) do |x, index|
+    h[index] = x
+  end
+
+  h.to_json
 end
