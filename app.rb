859b905708d88d5e29d20738408f3e9c5ac2950c
Anh Tran <anh.t.tran151@gmail.com>
2022-06-01 18:36:48 +0000

    test heroku database

--- app.rb
+++ app.rb
@@ -9,20 +9,20 @@ get '/' do
   erb :index
 end
 
-# get '/cake.json' do
-#   result = DB[:cakelist]
-#   return result.all.to_json
-# end
+get '/cake.json' do
+  result = DB[:cakelist]
+  return result.all.to_json
+end
 
-# get '/show/:id' do
-#   erb :show
-# end
+get '/show/:id' do
+  erb :show
+end
 
-# get '/edit/:id' do
-#   erb :edit
-# end
+get '/edit/:id' do
+  erb :edit
+end
 
-# post '/editsuccess/:id' do
-#   DB[:cakelist].where(id: params["id"]).update(name: params["cakename"])
-#   return "Success"
-# end
+post '/editsuccess/:id' do
+  DB[:cakelist].where(id: params["id"]).update(name: params["cakename"])
+  return "Success"
+end
