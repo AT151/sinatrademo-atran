635a9d928b0d689e35fcb1777dde4380d93a4d2e
Anh Tran <anh.t.tran151@gmail.com>
2022-06-01 17:30:57 +0000

    test

--- app.rb
+++ app.rb
@@ -3,26 +3,26 @@ require 'sinatra/json'
 require 'json'
 require 'sequel'
 
-DB = Sequel.connect('postgres://cakeuser:cakeuser@localhost:5432/cakes')
+# DB = Sequel.connect('postgres://cakeuser:cakeuser@localhost:5432/cakes')
 
 get '/' do
   erb :index
 end
 
-get '/cake.json' do
-  result = DB[:cakelist]
-  return result.all.to_json
-end
+# get '/cake.json' do
+#   result = DB[:cakelist]
+#   return result.all.to_json
+# end
 
-get '/show/:id' do
-  erb :show
-end
+# get '/show/:id' do
+#   erb :show
+# end
 
-get '/edit/:id' do
-  erb :edit
-end
+# get '/edit/:id' do
+#   erb :edit
+# end
 
-post '/editsuccess/:id' do
-  DB[:cakelist].where(id: params["id"]).update(name: params["cakename"])
-  return "Success"
-end
+# post '/editsuccess/:id' do
+#   DB[:cakelist].where(id: params["id"]).update(name: params["cakename"])
+#   return "Success"
+# end
