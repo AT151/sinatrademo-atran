e70d97de9f6e2e32d44dfbc3312e42b0c6d56ff1
Anh Tran <anh.t.tran151@gmail.com>
2022-06-02 13:24:04 +0000

    changed addsuccess to newsuccess in new.erb

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require 'sinatra/json'
 require 'json'
 require 'sequel'
 
-DB = Sequel.connect('postgres://fzkhgnzqiegvhz:7ede47178ef23ef790d2d99b6d46b9ea9c88a2172670f36202abc1dc85017339@ec2-34-227-120-79.compute-1.amazonaws.com:5432/dafanfgl4e086n')
-# DB = Sequel.connect('postgres://cakeuser:cakeuser@localhost:5432/cakes')
+# DB = Sequel.connect('postgres://fzkhgnzqiegvhz:7ede47178ef23ef790d2d99b6d46b9ea9c88a2172670f36202abc1dc85017339@ec2-34-227-120-79.compute-1.amazonaws.com:5432/dafanfgl4e086n')
+DB = Sequel.connect('postgres://cakeuser:cakeuser@localhost:5432/cakes')
 get '/' do
   erb :index
 end
@@ -22,13 +22,13 @@ get '/edit/:id' do
   erb :edit
 end
 
-get '/add' do
-  erb :add
+get '/new' do
+  erb :new
 end
 
-post '/addsuccess' do
+post '/newsuccess' do
   DB[:cakelist].insert(name: params["cakename"])
-  return erb :addsuccess
+  return erb :newsuccess
 end
 
 post '/editsuccess/:id' do
